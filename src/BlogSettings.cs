6826d5d8dde782a7fd71eac29cdec7a0ffdf00f5
Jordi Arquimbau <jarquimbau@chanlabs.com>
2020-01-15 22:18:07 +0000

    small corrections

--- src/BlogSettings.cs
+++ src/BlogSettings.cs
@@ -2,8 +2,9 @@
 {
     public class BlogSettings
     {
-        public string Owner { get; set; } = "Jordi Arquimbau";
+        public string Owner { get; set; } = "Jordi AG";
         public int PostsPerPage { get; set; } = 2;
         public int CommentsCloseAfterDays { get; set; } = 10;
+        public string Description { get; set; } = "";
     }
 }
